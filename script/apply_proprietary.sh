70f0e62f0a5add6bd180e2cbfe9d53f2d02678c3
yusw <yushengwei@e-planet.cn>
2019-06-30 10:35:44 +0800

    delete script

--- script/apply_proprietary.sh
+++
@@ -1,5 +0,0 @@
-export WORK=`pwd`
-export PKGS_DIR=$WORK/proprietary
-cd $WORK/sources/meta-renesas
-sh meta-rcar-gen3/docs/sample/copyscript/copy_evaproprietary_softwares.sh -f $PKGS_DIR
-unset PKGS_DIR
--- script/build_env.sh
+++
@@ -1,6 +0,0 @@
-export WORK=`pwd`
-source sources/poky/oe-init-build-env
-# h3ulcb|m3ulcb|m3nulcb|salvator-x|ebisu
-cp $WORK/sources/meta-renesas/meta-rcar-gen3/docs/sample/conf/salvator-x/linaro-gcc/mmp/*.conf ./conf/
-cd $WORK/build
-cp conf/local-wayland.conf conf/local.conf
